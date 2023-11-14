c9ca6d9559350852f112a520e890a4f4b6c613d1
Narayana56 <112703095+Narayana56@users.noreply.github.com>
2023-11-14 21:34:04 +0530

    Update Exp-15.py

--- Exp-15.py
+++ Exp-15.py
@@ -1,4 +1,5 @@
 print("DECISION TREE")
+print("sathya-192110689")
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
